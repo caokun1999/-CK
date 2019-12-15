2165b27f73a4c76e3b8178862226c01521e3642b
caokun1999 <1498395993@qq.com>
2019-12-15 13:14:07 +0800

    test2

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,7 @@
-public class Main {
+﻿public class Main {
 
     public static void main(String[] args) {
-        System.out.println("" +
-                "网络110报警服务\n" +
-                "\n" +
-                "北京互联网违法和不良信息举报中心\n" +
-                "\n" +
-                "中国互联网举报中心家长监护版权申诉" +
-                "999999");
+        System.out.println("酒酒");
 
     }
 }
